89b0b357c7e15ddfeb2f2bf29e6ff11e70fd110a
Jorge Iván <jrgz305@gmail.com>
2020-12-22 12:01:49 -0600

    login completo completo sin recuperar contraseña

--- resources/js/components/LoginComponent.vue
+++ resources/js/components/LoginComponent.vue
@@ -1,113 +1,3 @@
-<!-- <template>
-  <v-app id="inspire">
-    <v-system-bar app>
-      <v-spacer></v-spacer>
-
-      <v-icon>mdi-square</v-icon>
-
-      <v-icon>mdi-circle</v-icon>
-
-      <v-icon>mdi-triangle</v-icon>
-    </v-system-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <v-sheet
-        color="grey lighten-4"
-        class="pa-4"
-      >
-        <v-avatar
-          class="mb-4"
-          color="grey darken-1"
-          size="64"
-        ></v-avatar>
-
-        <div>john@vuetifyjs.com</div>
-      </v-sheet>
-
-      <v-divider></v-divider>
-
-      <v-list>
-        <v-list-item
-          v-for="[icon, text] in links"
-          :key="icon"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ text }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-main>
-      <v-container
-        class="py-8 px-6"
-        fluid
-      >
-        <v-row>
-          <v-col
-            v-for="card in cards"
-            :key="card"
-            cols="12"
-          >
-            <v-card>
-              <v-subheader>{{ card }}</v-subheader>
-
-              <v-list two-line>
-                <template v-for="n in 6">
-                  <v-list-item
-
-                    :key="n"
-                  >
-                    <v-list-item-avatar color="grey darken-1">
-                    </v-list-item-avatar>
-
-                    <v-list-item-content>
-                      <v-list-item-title>Message {{ n }}</v-list-item-title>
-
-                      <v-list-item-subtitle>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
-                      </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-
-                  <v-divider
-                    v-if="n !== 6"
-                    :key="`divider-${n}`"
-                    inset
-                  ></v-divider>
-                </template>
-              </v-list>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
-</template>
-
-<script>
-  export default {
-    data: () => ({
-      cards: ['Today', 'Yesterday'],
-      drawer: null,
-      links: [
-        ['mdi-inbox-arrow-down', 'Inbox'],
-        ['mdi-send', 'Send'],
-        ['mdi-delete', 'Trash'],
-        ['mdi-alert-octagon', 'Spam'],
-      ],
-    }),
-  }
-</script> -->
-
 <template>
   <v-app>
   
@@ -122,13 +12,13 @@
    
     <v-container fluid>
       <v-card
-        class="mx-auto my-1"
+        class="mx-auto"
         max-width="374"
       >
    
-   <img :src="'../public/img/profile.png'" width="100%" >
+   <img :src="'img/profile.png'" width="100%" >
 
-    <v-card-title>Ingresa con tus datos personales</v-card-title>
+    <v-card-title><h5><strong>Ingresa con tus datos personales</strong></h5></v-card-title>
     <validation-observer
     ref="observer"
     v-slot="{ invalid }"
@@ -179,24 +69,24 @@
       </v-row>
     </v-card-text>
 
-    <v-card-text class="d-flex justify-center">
-      <div>¿Olvidaste tu contraseña?</div>
-    </v-card-text>
-
     <v-card-actions>
      <v-btn color="primary" type="submit" block>
-    Ingresar
+    <strong>Ingresar</strong>
   </v-btn>
       
     </v-card-actions>
   </form>
   </validation-observer>
+
+  <v-card-text class="d-flex justify-center">
+      <div>¿Olvidaste tu contraseña?</div>
+    </v-card-text>
   </v-card>
     </v-container>
   </v-main>
 
   <v-footer app class="d-flex justify-center">
-    <!-- -->Sistema de planificación de recursos empresariales- Gsureste ©{{ new Date().getFullYear() }} Derechos Reservados
+    <strong>Sistema de planificación de recursos empresariales © {{ new Date().getFullYear() }}</strong>
   </v-footer>
 </v-app>
 </template>
@@ -205,7 +95,7 @@
   const axios = require('axios');
   import { required, digits, email, max, regex } from 'vee-validate/dist/rules'
   import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
-  // import swal from 'sweetalert';
+  import swal from 'sweetalert';
 
   setInteractionMode('eager')
   extend('digits', {
@@ -270,7 +160,7 @@
       })
       .catch(function (error) {
 
-       // swal("Acceso incorrecto!", "Ingresa los datos de usuario correctos!", "warning");
+       swal("Acceso incorrecto!", "Ingresa los datos de usuario correctos!", "warning");
 
       })
 
